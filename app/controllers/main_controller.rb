b5d5d0b178ee77a16d66af307106f3dc70d7cc01
Guillermo <guillhermoherrera@gmail.com>
2020-10-29 12:36:33 -0600

    root authenticated & unauthenticated

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,8 @@
 class MainController < ApplicationController
   def home
   end
+
+  def unregistered
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'main#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  authenticated :user do
+    root 'main#home'
+  end
+
+  unauthenticated :user do
+    root 'main#unregistered', as: :unregistered
+  end
 end
