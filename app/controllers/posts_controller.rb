7cecc12635eabd8473d3144b4b6d4b93fc6b70db
Guillermo <guillhermoherrera@gmail.com>
2020-10-30 13:49:07 -0600

    instalacion action_text & add to scaffold
    rails action_text:install
    rails db:migrate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:titulo, :user_id, :status)
+      params.require(:post).permit(:titulo, :user_id, :status, :contenido)
     end
 end
