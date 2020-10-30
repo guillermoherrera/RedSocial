df16bef5fd18ca089d213a55702221179df733ea
Guillermo <guillhermoherrera@gmail.com>
2020-10-30 10:05:47 -0600

    separar landing de aplication

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,13 @@ class MainController < ApplicationController
   def home
   end
 
-  def unregistered
-    
+  def unregistered    
+  end
+
+  protected
+  
+  def set_layout
+    return 'landing' if action_name == 'unregistered'
+    super
   end
 end
