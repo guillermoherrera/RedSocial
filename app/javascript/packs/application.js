bf89195e6a71f1c76b510527396c190efa6841ff
Guillermo <guillhermoherrera@gmail.com>
2020-10-31 13:30:48 -0600

    add Icons font awesome
    yarn add @fortawesome/fontawesome-free

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ require("jquery")
 
 import 'bootstrap'
 import 'css/styles'
+import "@fortawesome/fontawesome-free/js/all";
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
