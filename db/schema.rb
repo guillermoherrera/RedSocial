b0f5543a7da553e8f1e3322a531a2b1cebcbfed2
Guillermo <guillhermoherrera@gmail.com>
2020-11-03 09:40:41 -0600

    create model Friendship
    rails g model Friendship user:references friend:references status
    
    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_154035) do
+ActiveRecord::Schema.define(version: 2020_11_03_153512) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 2020_11_02_154035) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "titulo"
     t.integer "user_id", null: false
@@ -68,5 +78,7 @@ ActiveRecord::Schema.define(version: 2020_11_02_154035) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "friendships", "friends"
+  add_foreign_key "friendships", "users"
   add_foreign_key "posts", "users"
 end
